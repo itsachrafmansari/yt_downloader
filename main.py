d3176aac876ea72dcde8eb4d0fa9bbf1153c2824
Achraf Mansari <achraf.mansari.1@gmail.com>
2021-01-27 23:11:19 +0100

    Fix : Convert audio files from .webm to .mp3 YouTube streams audio files in WEBM format, as a result videos downloaded as audio files have WEBM format, so we have to convert them to MP3 format using PyDub library. OS used to define the path of the audio files. Safe_filename used to create system-friendly filenames for the audio files

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import os
+from pytube.helpers import safe_filename
+from pydub import AudioSegment
 from pytube import YouTube, Playlist
 
-blankline = '_' * 70 + '\n \n' #Some decoration !
-dlpath = r'C:\Users\Your Username\Downloads' #Choose a download folder
+blankline = '_' * 70 + '\n \n'   # Some decoration !
+dlpath = r'C:\Users\Your Username\Downloads'  # Choose a download folder
+
+# Coverts downloaded audio files from there original format .webm to .mp3
+def convertaudio(var):
+    webmaudio = os.path.join(dlpath, safe_filename(var.title) + '.webm')
+    mp3audio = os.path.join(dlpath, safe_filename(var.title) + '.mp3')
+    AudioSegment.from_file(webmaudio, "webm").export(mp3audio, format="mp3")
+    os.remove(webmaudio)
 
 #Defining the download function
-def dl(thevideo):
-    print('Video titled [' + thevideo.title + '] started downloading')
+def dl(var):
+    print('Video titled [' + var.title + '] started downloading')
     if Format == 'v' or Format == 'V':
-        thevideo.streams.filter(subtype='mp4', progressive=True).order_by('resolution').last().download(dlpath)
+        var.streams.filter(subtype='mp4', progressive=True).order_by('resolution').last().download(dlpath)
     if Format == 'a' or Format == 'A':
-        thevideo.streams.filter(only_audio=True, subtype='webm').order_by('bitrate').last().download(dlpath)
-    print('Video titled [' + thevideo.title + '] has been downloaded')
+        var.streams.filter(only_audio=True, subtype='webm').order_by('bitrate').last().download(dlpath)
+    print('Video titled [' + var.title + '] has been downloaded')
 
 #Some inputs
 Type = input('What yo want to download ? [v] for video and [p] for playlist : ')
