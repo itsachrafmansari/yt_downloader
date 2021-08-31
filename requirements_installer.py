5d9ceec9f631e0e2fbc2e3cd1238c733af15df69
Achraf Mansari <contact@achrafmansari.com>
2021-08-31 06:20:33 +0100

    Cleaned the requirements installer

--- requirements_installer.py
+++ requirements_installer.py
@@ -1,14 +1,3 @@
-import os
-
-# Styling
-bl = "echo. & "
-equ = "="
-
-# Creating the command line
-cmd = 'color A0 & ' + bl*3 + 'echo "' + equ*15 + ' Installing requirements ' + equ*15 + '" & ' + bl*2 + 'echo. & '
-cmd = cmd + 'pip install -U -r requirements.txt & ' + bl * 4
-cmd = cmd + 'echo "' + equ*15 + ' Done ! ' + equ*15 + '" & ' + bl*3 + 'echo.'
-cmd = 'cmd /k "' + cmd + '"'
-
-# Executing the command line
-os.system(cmd)
+import sys
+import subprocess
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade',  'pytube', 'ffmpeg-python', 'eel'])
